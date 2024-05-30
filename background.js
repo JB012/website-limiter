822b58a7a1146cb2a0ca19e40bdac383bdf511be
JB012 <80133647+JB012@users.noreply.github.com>
2024-05-30 15:42:04 -0400

    Add files via upload
    Added Chrome notifications for the limits left.

--- background.js
+++ background.js
@@ -7,14 +7,13 @@ chrome.tabs.onCreated.addListener(() => {
         const hour = new Date().getHours();
 
         if (website && url) {
+            console.log(`Website:${website.key}, tab: ${url}, newLimit:${website.newLimit}`)
             if (url.includes(website.key)) {
                 if (hour !== 0 || hour !== 24) {
                     if (website.newLimit === 0) {
                         chrome.tabs.remove(tabId);
 
                         //Remove open tabs of the website.
-
-
                         chrome.tabs.query({}, function(tabs) {
                             for (const tab of tabs) {
                                 if (tab.url.includes(website.key)) {
@@ -25,6 +24,13 @@ chrome.tabs.onCreated.addListener(() => {
                     }
                     else {
                         chrome.storage.local.set({newLimit: website.newLimit - 1});
+                        chrome.notifications.create("LimitMessage", 
+                        {   type: "basic",
+                            iconUrl: "icon32.png",
+                            title: 'LimitMessage',
+                            message: `Limit left: ${website.newLimit - 1}`
+                        });
+                        chrome.notifications.clear("LimitMessage");
                     }
                 }
                 else {
